a9a449bf045d0a95ff022435189d3c7c010ec960
tacman <tacman@users.noreply.github.com>
2026-05-12 14:09:33 +0000

    refactor all commands to remove execute(), new contracts, etc.

--- src/Command/LoadDummyCommand.php
+++ src/Command/LoadDummyCommand.php
@@ -6,38 +6,24 @@ namespace Survos\DummyBundle\Command;
 
 use Survos\DummyBundle\Service\DummyLoader;
 use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Attribute\Option;
 use Symfony\Component\Console\Command\Command;
-use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Input\InputOption;
-use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
 
-#[AsCommand(
-    name: 'dummy:load',
-    description: 'Load the full DummyJSON graph into Doctrine entities'
-)]
-final class LoadDummyCommand extends Command
+#[AsCommand('dummy:load', 'Load the full DummyJSON graph into Doctrine entities')]
+final class LoadDummyCommand
 {
     public function __construct(
         private readonly DummyLoader $dummyLoader,
     ) {
-        parent::__construct();
     }
 
-    protected function configure(): void
-    {
-        $this->addOption('purge', null, InputOption::VALUE_NONE, 'Purge existing rows before import');
-    }
-
-    protected function execute(InputInterface $input, OutputInterface $output): int
-    {
-        $io = new SymfonyStyle($input, $output);
-        $purge = (bool) $input->getOption('purge');
-
+    public function __invoke(
+        SymfonyStyle $io,
+        #[Option('Purge existing rows before import')] bool $purge = false,
+    ): int {
         $io->title('Load DummyJSON data');
-        $io->definitionList([
-            'Purge' => $purge ? 'yes' : 'no',
-        ]);
+        $io->definitionList(['Purge' => $purge ? 'yes' : 'no']);
 
         $counts = $this->dummyLoader->loadAll($purge);
 
